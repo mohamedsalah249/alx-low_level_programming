6300db765290b2dc42cf81578db6d015df848f2a
mohamedsalah249 <mohammedsalah511.ms@gmail.com>
2024-06-26 19:18:08 -0700

    8/24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,23 +1,24 @@
 #include "main.h"
 
 /**
- * jack_bauer - Prints every minute of the day of Jack Bauer, from 00:00 to 23:59
+ * jack_bauer - Prints every minute of the day of Jack Bauer.
+ *             From 00:00 to 23:59, each time is printed in the format "HH:MM".
  */
 void jack_bauer(void)
 {
-	int hour, minute;
+    int hour, minute;
 
-	for (hour = 0; hour < 24; hour++)
-	{
-		for (minute = 0; minute < 60; minute++)
-		{
-			_putchar('0' + (hour / 10)); /* Print tens digit of hour */
-			_putchar('0' + (hour % 10)); /* Print ones digit of hour */
-			_putchar(':');
-			_putchar('0' + (minute / 10)); /* Print tens digit of minute */
-			_putchar('0' + (minute % 10)); /* Print ones digit of minute */
-			_putchar('\n'); /* Newline after each minute */
-		}
-	}
+    for (hour = 0; hour < 24; hour++)
+    {
+        for (minute = 0; minute < 60; minute++)
+        {
+            _putchar('0' + (hour / 10)); /* Print tens digit of hour */
+            _putchar('0' + (hour % 10)); /* Print ones digit of hour */
+            _putchar(':');
+            _putchar('0' + (minute / 10)); /* Print tens digit of minute */
+            _putchar('0' + (minute % 10)); /* Print ones digit of minute */
+            _putchar('\n'); /* Newline after each minute */
+        }
+    }
 }
 
