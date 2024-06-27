5086be72fbaf9212b7060c5c0fcc208769b0fa25
mohamedsalah249 <mohammedsalah511.ms@gmail.com>
2024-06-26 19:06:11 -0700

    7-print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,14 @@
  */
 int print_last_digit(int n)
 {
-    int last_digit;
+	int last_digit;
 
-    if (n < 0)
-        last_digit = -1 * (n % 10); /* Get positive last digit */
-    else
-        last_digit = n % 10;
+	if (n < 0)
+		last_digit = -1 * (n % 10); /* Get positive last digit */
+	else
+		last_digit = n % 10;
 
-    _putchar('0' + last_digit); /* Print last digit */
-    return last_digit;
+	_putchar('0' + last_digit); /* Print last digit */
+	return (last_digit); /* Return last digit */
 }
 
